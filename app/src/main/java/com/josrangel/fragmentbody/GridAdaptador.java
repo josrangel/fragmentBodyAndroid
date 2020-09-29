53042259215548b7a71a722a7617c30f6c3099a8
josue rangel <josrangel@hotmail.com>
2020-09-29 16:02:16 -0500

    avance

---
+++ app/src/main/java/com/josrangel/fragmentbody/GridAdaptador.java
@@ -0,0 +1,34 @@
+package com.josrangel.fragmentbody;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class GridAdaptador extends BaseAdapter {
+
+    Context context;
+    ArrayList imagenes;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
